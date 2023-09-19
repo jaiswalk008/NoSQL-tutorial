c7e11f59722709e91319cb51efb4874ff309236f
jaiswalk008 <jaiskaran008@gmail.com>
2023-09-19 19:50:29 +0530

    connected to mongodb and created add-product api

---
+++ models/product.js
@@ -0,0 +1,25 @@
+const getDb = require('../util/database').getDb;
+
+class Product {
+  constructor( title, price, description, imageUrl ) {
+     
+    this.title = title;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.description = description;
+  }
+
+  save() {
+    const db = getDb();
+    return db.collection('products')
+      .insertOne(this)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+
+module.exports = Product;
